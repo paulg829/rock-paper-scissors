a6d1126fdcbe3d3dad628ea237c84b7c541a05c2
paul829 <pgarofalo880@gmail,com>
2026-07-31 13:40:40 -0500

    Finished computer choice function

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("Hello World");
+
+
+function getComputerChoice() {
+
+    let randomNumber = Math.floor(Math.random() * 3);
+
+    if (randomNumber === 0) {
+
+        return "rock";
+    }
+
+    else if (randomNumber === 1) {
+
+        return "paper";
+    }
+
+    else {
+        
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
+
+
