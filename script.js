e1f0f18cab18a9eebcf145c6aa458fc7ccf6432f
paul829 <pgarofalo880@gmail,com>
2026-07-31 14:13:37 -0500

    One round of rock-paper-scissors

--- script.js
+++ script.js
@@ -32,3 +32,41 @@ function getHumanChoice() {
 }
 
 console.log(getComputerChoice());
+
+function playRound (humanChoice, computerChoice) {
+
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice === computerChoice) {
+        console.log("Tie!");
+    }
+
+    else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    )
+      {
+
+        humanScore ++;
+
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+
+    }
+
+    else {
+
+        computerScore ++;
+
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    }
+
+    console.log(`Human: ${humanScore}`);
+    console.log(`Computer: ${computerScore}`);
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
